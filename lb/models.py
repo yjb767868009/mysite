3f5b32860ef55befb3e2f556e30a741aadfdb1bd
fish <767868009@qq.com>
2018-01-28 15:12:24 +0800

    try to model

--- lb/models.py
+++ lb/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class category(models.Model):
+    name = models.CharField(max_length=100)
+    des = models.CharField(max_length=200)
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    signature = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    reg_time = models.DateTimeField()
+    department = models.CharField(max_length=100)
+    website = models.URLField()
+
+class Environment(models.Model):
+    name = models.CharField(max_length=100)
+    short_des = models.CharField(max_length=200)
+    long_des = models.TextField()
+    solved = models.CharField(max_length=10)
+    passing_line = models.IntegerField(default=0)
+    category = models.ManyToManyField(category)
+
+class Submission(models.Model):
+    name = models.CharField(max_length=100)
+    des = models.TextField()
+    sub_date = models.DateTimeField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    envir = models.ForeignKey(Environment,on_delete=models.CASCADE)
+
